0c64fa9d34d8403606b360900a3414a686dc604a
AVERAGEGAEL <michaelle@sunrisemiddle.org>
2025-03-19 14:43:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById('goButton').addEventListener('click', () => {
+  const url = document.getElementById('urlInput').value;
 
+  if (url) {
+    // Use a CORS proxy to bypass restrictions
+    const proxyUrl = `https://cors-anywhere.herokuapp.com/${url}`;
+
+    // Set the iframe source to the proxied URL
+    const proxyFrame = document.getElementById('proxyFrame');
+    proxyFrame.src = proxyUrl;
+    proxyFrame.style.display = 'block';
+  } else {
+    alert('Please enter a valid URL.');
+  }
+});
